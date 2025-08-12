99f85c7d13f12d495a76ceca7a9c294947248c21
PortelaZod <jeffersonporela@live.com>
2025-08-11 22:15:20 -0400

    novo endpoint

--- src/main/java/com/monginho/monginho/UserController.java
+++ src/main/java/com/monginho/monginho/UserController.java
@@ -3,12 +3,13 @@ package com.monginho.monginho;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
 
 
 
@@ -35,5 +36,9 @@ public class UserController {
         return this.userRepository.findById(userId).get();
     }
     
-
+    @GetMapping("/outro")
+    public String getMethodName2() {
+        return "funciona";
+    }
+    
 }
